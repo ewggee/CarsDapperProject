93acd0d090b52c5bea9dde9892295bb880f299d7
Евгений Кузнецов <egions2006@gmail.com>
2025-05-20 20:21:00 +0300

    refactor: удалён ненужый комментарий в миграции M001_CreateTables

--- CarsDapperProject.Domain/Migrations/M001_CreateTables.cs
+++ CarsDapperProject.Domain/Migrations/M001_CreateTables.cs
@@ -7,11 +7,6 @@ public class M001_CreateTables : Migration
 {
     public override void Up()
     {
-        //Execute.Sql(@"
-        //    DROP TABLE IF NOT EXISTS brands CASCADE;
-        //    DROP TABLE IF NOT EXISTS cars CASCADE;
-        //    DROP TABLE IF NOT EXISTS owners CASCADE;");
-
         Create.Table("brands")
             .WithColumn("id").AsInt32().PrimaryKey().Identity()
             .WithColumn("name").AsString(100).NotNullable();
