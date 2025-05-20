212bd190913eb6574cc96133c5335297212ec3a4
Евгений Кузнецов <egions2006@gmail.com>
2025-05-20 11:19:39 +0300

    feature: добавлена валидация запроса для добавления и обновления бренда

--- CarsDapperProject.Core/DTOs/CreateBrandRequest.cs
+++ CarsDapperProject.Core/DTOs/CreateBrandRequest.cs
@@ -4,5 +4,7 @@ namespace CarsDapperProject.Application.DTOs;
 
 public class CreateBrandRequest
 {
+    [Required(ErrorMessage = $"Поле {nameof(Name)} обязательно.")]
+    [Length(1, 100, ErrorMessage = $"Поле {nameof(Name)} должно быть от 1 до 100 символов.")]
     public string Name { get; set; }
 }
--- CarsDapperProject.Core/DTOs/UpdateBrandRequest.cs
+++ CarsDapperProject.Core/DTOs/UpdateBrandRequest.cs
@@ -4,5 +4,6 @@ namespace CarsDapperProject.Application.DTOs;
 
 public class UpdateBrandRequest
 {
+    [Length(1, 100, ErrorMessage = $"Поле {nameof(Name)} должно быть от 1 до 100 символов.")]
     public string Name { get; set; }
 }
