f04c1f6783d336df2a1a117a91e0e549c4e6e864
Евгений Кузнецов <egions2006@gmail.com>
2025-05-22 20:33:04 +0300

    refactor: добавлен кастомный атрибут для ограничений по значению поля

---
+++ CarsDapperProject.Contracts/Attributes/RangeField.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarsDapperProject.Contracts.Attributes;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+public class RangeField(double min, double max) : RangeAttribute(min, max)
+{
+    public override string FormatErrorMessage(string name)
+    {
+        if (string.IsNullOrEmpty(ErrorMessage))
+        {
+            ErrorMessage = $"Поле {name} должно иметь значение в промежутке от {Minimum} до {Maximum}";
+        }
+
+        return base.FormatErrorMessage(name);
+    }
+}
